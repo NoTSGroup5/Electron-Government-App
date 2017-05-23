9b6691c0e9b8401b8d86984a9796c977f8078cdc
Sander Koenders <sanderkoenders@gmail.com>
2017-05-23 11:07:28 +0200

    Refactor(change): Small change

--- app/src/renderer/components/Shared/Bootstrap/BootstrapTextInput.vue
+++ app/src/renderer/components/Shared/Bootstrap/BootstrapTextInput.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="form-group row">
-        <label :for="type + '_' + name" class="col-sm-4 col-form-label" :aria-describedby="type + '_' + name">{{ name }}</label>
-        <div class="col-sm-10">
-            <input :type="type" class="form-control" :id="type + '_' + name" placeholder="Address">
+        <label :for="type + '_' + name" class="col-sm-4 col-form-label" :aria-describedby="type + '_' + name">{{ value }}</label>
+        <div class="col-sm-8">
+            <input :type="type" class="form-control" :id="type + '_' + name" :placeholder="value">
             <small v-if="help" class="form-text text-muted">{{ help }}</small>
         </div>
     </div>
@@ -10,6 +10,6 @@
 
 <script>
   export default {
-    props: ['type', 'name', 'help']
+    props: ['type', 'name', 'help', 'value']
   }
 </script>
