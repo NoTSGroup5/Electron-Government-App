dec7d287b0f75e7f15aaed56972962c4300e2cb8
Sander Koenders <sanderkoenders@gmail.com>
2017-05-23 12:17:16 +0200

    Feat(Modal): Add component for bootstrap modal

---
+++ app/src/renderer/components/Shared/Bootstrap/BootstrapModal.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="modal fade" :id="modalId" tabindex="-1" role="dialog" :aria-labelledby="ariaLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">{{ title }}</h4>
+                </div>
+                <div class="modal-body">
+                    <slot></slot>
+                </div>
+                <div class="modal-footer">
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Opslaan</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      props: {
+        title: {
+          type: String,
+          default: 'Modal'
+        },
+        modalId: {
+          type: String,
+          required: true,
+        },
+        ariaLabel: {
+          type: String,
+          default: 'myModal'
+        }
+      }
+    }
+</script>
