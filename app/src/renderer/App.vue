7c05346f99f72566eb94fb76194678f1c7372a9d
Sander Koenders <sanderkoenders@gmail.com>
2017-05-23 13:55:02 +0200

    Implement Navbar in application

--- app/src/renderer/App.vue
+++ app/src/renderer/App.vue
@@ -1,50 +1,45 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">EPD</a>
-        </div>
+    <div id="app">
+        <Navbar title="EPD">
+            <NavbarLink to="/">Home</NavbarLink>
+            <NavbarLink to="/patients">Patients</NavbarLink>
+            <NavbarLink to="/organisations">Organisaties</NavbarLink>
+        </Navbar>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="patients">Patients</router-link></li>
-            <li><router-link to="organisations">Organisaties</router-link></li>
-          </ul>
+        <div class="container">
+            <router-view></router-view>
         </div>
-      </div>
-    </nav>
-
-    <div class="container">
-      <router-view></router-view>
     </div>
-  </div>
 </template>
 
 <script>
   import store from 'renderer/vuex/store';
+  import Navbar from './components/Shared/Bootstrap/Navbar/Navbar';
+  import NavbarLink from './components/Shared/Bootstrap/Navbar/NavbarLink'
+
   export default {
-    store
+    store,
+
+    components: {
+      Navbar,
+      NavbarLink
+    }
   }
 </script>
 
 <style>
-  @import url(https://fonts.googleapis.com/css?family=Lato:300);
+    @import url(https://fonts.googleapis.com/css?family=Lato:300);
 
-  * {
-    margin: 0;
-    padding: 0;
-  }
+    * {
+        margin: 0;
+        padding: 0;
+    }
 
-  html,
-  body { height: 100%; width:100% }
+    html,
+    body {
+        height: 100%;
+        width: 100%
+    }
 
 
 </style>
