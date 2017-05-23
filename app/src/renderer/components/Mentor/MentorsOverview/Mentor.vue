ec083a85670ff9cad434138b552e23a8bf76a4e7
Parwiz <p.ahmadi90@outlook.com>
2017-05-23 10:26:20 +0200

    Patient bewerken page fix

---
+++ app/src/renderer/components/Mentor/MentorsOverview/Mentor.vue
@@ -0,0 +1,13 @@
+<template>
+    <tr>
+        <td>{{ BSN }}</td>
+        <td>{{ Naam }}</td>
+        <td><button class="btn btn-default">Bewerk</button></td>
+    </tr>
+</template>
+
+<script>
+  export default {
+    props: ['BSN', 'Naam']
+  }
+</script>
