be673df1252c746cfb52023ff57b9532906314fd
Sander Koenders <sanderkoenders@gmail.com>
2017-05-23 13:08:01 +0200

    Feat(Modal): Add optional slot for header and footer

--- app/src/renderer/components/Shared/Bootstrap/BootstrapModal.vue
+++ app/src/renderer/components/Shared/Bootstrap/BootstrapModal.vue
@@ -3,16 +3,18 @@
         <div class="modal-dialog" :class="modalClass" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">{{ title }}</h4>
+                    <slot name="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">{{ title }}</h4>
+                    </slot>
                 </div>
                 <div class="modal-body">
                     <slot></slot>
                 </div>
                 <div class="modal-footer">
-                    <div class="modal-footer">
+                    <slot name="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Opslaan</button>
-                    </div>
+                    </slot>
                 </div>
             </div>
         </div>
