8bda46f1ee39783cf863bfa337c48b3a5d597d53
Sander Koenders <sanderkoenders@gmail.com>
2017-05-23 10:40:52 +0200

    Feat(OrganisationList): Add button to add organisations
    This button does not work at this moment, still trying to figure out how to make a link on a button in vue.js

--- app/src/renderer/components/Organization/OrganizationOverview.vue
+++ app/src/renderer/components/Organization/OrganizationOverview.vue
@@ -1,16 +1,36 @@
 <template>
     <div>
-        <h1>Organisaties</h1>
-        <OrganizationList></OrganizationList>
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Organisaties</h1>
+            </div>
+            <div class="col-md-6 text-right">
+                <button class="btn-default btn btn-match-h1">Maak Organisatie</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <OrganizationList></OrganizationList>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
   import OrganizationList from './OrganizationOverview/OrganizationList.vue'
+
   export default {
+    name: 'organizationOverview',
+
     components: {
       OrganizationList
-    },
-    name: 'organizationOverview'
+    }
   }
 </script>
+
+<style scoped>
+    .btn-match-h1 {
+        margin-top: 20px;
+        margin-bottom: 10px;
+    }
+</style>
