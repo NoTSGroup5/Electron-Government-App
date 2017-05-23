ac7b0db25d20712c75fe463673031db2ca87db75
Sander Koenders <sanderkoenders@gmail.com>
2017-05-23 09:49:52 +0200

    feat(Helper): Add bootstrap inputTextField Helper

---
+++ app/src/renderer/components/Shared/Bootstrap/BootstrapTextInput.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="form-group row">
+        <label for="{{ type }}_{{ name }}" class="col-sm-4 col-form-label" aria-describedby="{{ type }}_{{ name }}">{{ name }}</label>
+        <div class="col-sm-10">
+            <input type="{{ type }}" class="form-control" id="{{ type }}_{{ name }}" placeholder="Address">
+            <small id="{{ type }}_{{ name }}" class="form-text text-muted">{{ help }}</small>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: ['type', 'name', 'help']
+  }
+</script>
